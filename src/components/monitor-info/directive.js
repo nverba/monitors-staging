79596084a088f86182dfda1e1e677eb16f79da18
nverba <distantstate@gmail.com>
2015-09-30 08:58:46 +0100

    updated info

--- src/components/monitor-info/directive.js
+++ src/components/monitor-info/directive.js
@@ -9,6 +9,16 @@ function monitorInfoFn() {
 		scope: { "monitor": "=monitorInfo" },
 		templateUrl: './src/components/monitor-info/template.html',
 		link: function (scope, elem, attrs) {
+			
+			console.log(scope.monitor);
+			
+			scope.uptimes = {};
+			var upkey = [1, 2, 7, 14, 30];
+			var splituptime =  scope.monitor.customuptimeratio.split('-');
+			
+			for (var u = 0; u < 5; u++) {
+				scope.uptimes[upkey[u]] = splituptime[u];
+			}
 
 			var logs = scope.monitor.log.reverse();
 			scope.incidents = [];
